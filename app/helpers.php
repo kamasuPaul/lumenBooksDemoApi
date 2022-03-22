a606546f38294ac6f91a9f662f775659f0136e34
Kamasu Paul <kamasupaul@gmail.com>
2022-03-22 15:07:51 +0300

    -- add spatie query builder

---
+++ app/helpers.php
@@ -0,0 +1,12 @@
+<?php
+if (! function_exists('config_path')) {
+    /**
+     * Get the path to the configuration files.
+     *
+     * @return string
+     */
+    function config_path()
+    {
+        return app()->basePath().'/config';
+    }
+}
